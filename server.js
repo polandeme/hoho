ba639e90d6e327115b1b5326e080156d6708ec1d
polandeme <polandeme@gmail.com>
2014-09-06 23:36:57 +0800

    change url

--- server.js
+++ server.js
@@ -13,28 +13,40 @@ io.on('connection', function(socket) {
 	socket.on('send', function(msg) {
 		var data = '';
 		console.log(msg.content);
-		http1.get(msg.content, function(res) {
-			console.log('-------------');
-			res.setEncoding('utf8');
+		var b_url = /^(((ht|f)tp(s?))\:\/\/)?(www.|[a-zA-Z].)[a-zA-Z0-9\-\.]+\.(com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk)(\:[0-9]+)*(\/($|[a-zA-Z0-9\.\,\;\?\'\\\+&amp;%\$#\=~_\-]+))*$/.test(msg.content); 
+		console.log(b_url);
 
-			res.on('data', function (chunk) {
-				data += chunk;
-			});
+		if(b_url) {
+			if(/(http(s))/.test(msg.content)) {
+				console.log('ture');
+				var url = msg.content;
+			} else {
+				var url = 'http:\\\\' + msg.content;
+			}
+			console.log(url);
+			http1.get(url, function(res) {
+				console.log('-------------');
+				res.setEncoding('utf8');
 
-			res.on('end', function() {
-				console.log('======');
-				var html = data.toString();
-				var $ = cheerio.load(html);
-				io.emit('send', $('title').text());
-			})
-			.on('error', function() {
-				console.log('-=-=-=-=-');
-				io.emit('send', 'error');
+				res.on('data', function (chunk) {
+					data += chunk;
+				});
+
+				res.on('end', function() {
+					console.log('======');
+					var html = data.toString();
+					var $ = cheerio.load(html);
+					io.emit('send', $('title').text());
+				})
+				.on('error', function() {
+					console.log('-=-=-=-=-');
+					io.emit('send', 'error');
+				});
 			});
-			// console.log(res.statusCode);
-			// io.emit('send', res.statusCode);
-		});
-		// io.emit('send', msg);	
+		} else{
+			io.emit('send', msg.content);
+		}
+		
 	});
 
 });
