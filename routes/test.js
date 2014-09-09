db09ae7d3a0e1fb73b894b844c097c4c870e9283
polandeme <polandeme@gmail.com>
2014-09-09 23:21:45 +0800

    add mysql connect

--- routes/test.js
+++ routes/test.js
@@ -3,14 +3,26 @@ var multipart = require('connect-multiparty');
 var fs = require('fs');
 var multipartMiddleware = multipart();
 var router  = express.Router();
+var mysql = require('mysql');
+var client = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password:''
+});
 
 router.post('/', multipartMiddleware, function(req,res) {
 	console.log(req.files.file.path);
 	var tmp_path = req.files.file.path;
 	var target_path = __dirname + req.files.file.name;
-	// console.log(req.body);
 	fs.readFile(tmp_path, function (err, data){
 	    fs.writeFile(target_path, data, function (err) {
+	    	client.query('CREATE DATABASE node', function(err) {
+	    		if(err){
+	    			console.log('-d-d-d-d-d');
+	    			client.end();
+	    			throw err;
+	    		}
+	    	});
 	    	console.log(data);
 		    if(err){
 		    	console.log('------');
@@ -19,10 +31,6 @@ router.post('/', multipartMiddleware, function(req,res) {
 			}
 		})
 	})
-	// console.log(target_path);
-// console.log(req.file);
-// res.send('hello test');
-
 });
 
 module.exports = router;
